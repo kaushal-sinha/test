bac61108bc96983f6166cf74cd04bdc0243ea465
PDC4-Training.PDC4 <PDC4-Training.PDC4@P4A-508SD52.dir.svc.accenture.com>
2018-07-27 10:40:22 +0530

    commit 3

--- MyFramework/src/test/java/com/accenture/cucumbertest/RunnerClass.java
+++ MyFramework/src/test/java/com/accenture/cucumbertest/RunnerClass.java
@@ -3,6 +3,10 @@ package com.accenture.cucumbertest;
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import cucumber.api.junit.Cucumber;
 import junit.framework.TestCase;
 
@@ -11,6 +15,26 @@ import junit.framework.TestCase;
 @CucumberOptions(features ="C:\\git_lip\\MyFramework\\src\\test\\java\\com\\accenture\\cucumbertest\\login.feature")
 
 public class RunnerClass extends TestCase {
+	@Given("^I am on login page$")
+	public void i_am_on_login_page() throws Throwable {
+		System.out.println("Hello Given");
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^I give tomsmith and SuperSecretPassword!$")
+	public void i_give_tomsmith_and_SuperSecretPassword() throws Throwable {
+		System.out.println("Hello When");
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^I should be login$")
+	public void i_should_be_login() throws Throwable {
+		System.out.println("Hello Then");
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
 
 	
 
