ccd1e225fcf58314ab0b4d284924311d0dd92d50
KSinha <34678521+kaushal-sinha@users.noreply.github.com>
2018-07-27 06:22:56 +0000

    Update Demos_Addnum.java

--- Demos/src/day1/Demos_Addnum.java
+++ Demos/src/day1/Demos_Addnum.java
@@ -3,6 +3,7 @@ package day1;
 public class Demos_Addnum {
 	public static void main(String args[])
 	{
+		/* EDited by Kaushal Sinha 27 July 2018 11:52 AM IST */
 		int num1,num2,sum,multiply;
 		num1=10;
 		num2=20;
