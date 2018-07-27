e4d2665875c5958869f814a3d9f79f7ecf62768c
PDC4-Training.PDC4 <PDC4-Training.PDC4@P4A-508SD52.dir.svc.accenture.com>
2018-07-27 11:01:31 +0530

    commit

---
+++ Demos/src/day1/Demos_Addnum.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class Demos_Addnum {
+	public static void main(String args[])
+	{
+		int num1,num2,sum,multiply;
+		num1=10;
+		num2=20;
+		sum=num1+num2;
+		multiply=num1*num2;
+		System.out.println("Sum="+sum);
+		System.out.println("Multiplicatio="+multiply);
+	}
+
+}
